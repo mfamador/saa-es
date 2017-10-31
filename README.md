d216a2c4345cb2fd19db201ea72e9d81a99bac15
Marco Amador <amador.marcoggmail.com>
2017-10-31 14:01:48 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sample ES image
+
---
+++ provision.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -e
+
+# Start up Elasticsearch
+/usr/share/elasticsearch/bin/es-docker &
+# newer ES docker image version uses this entrypoint instead
+# /usr/local/bin/docker-entrypoint.sh eswrapper &
+
+set +e
+
+# Delete all 'junk' indices
+curl -XDELETE localhost:9200/_all
+
+# Add index settings
+while true; do
+	curl -fsSL -XPUT -H "Content-Type: application/json" --data-binary @/tmp/mappings.json http://localhost:9200/documents
+	if [ $? == 0 ]; then
+		break
+	fi
+	sleep 1
+done
+
+# Import our data
+while true; do
+	curl -fsSL -XPOST -H "Content-Type: application/json" --data-binary @/tmp/data.json http://localhost:9200/documents/document/_bulk
+	if [ $? == 0 ]; then
+		break
+	fi
+	sleep 10
+done
+set -e
+
+# Shut down Elasticsearch so it's data files are in a clean state
+kill $(ps -ef | grep java | grep -v grep | awk '{print $2}')
+
+# Wait on all child processes to exit (i.e. Elasticseach)
+wait
